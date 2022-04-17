953237048a800385411b0c72c8d4ccdbc2829ca6
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-04-17 21:43:44 +0530

    feat: added init command to cli

--- rfc.ts
+++ rfc.ts
@@ -27,4 +27,75 @@ if (args[0] === "gen") {
   while (true);
 } else if (args[0] === "init") {
   console.log("Scaffolding the app");
+  console.info("Writing deno.json");
+  await Deno.writeTextFile(
+    "deno.json",
+    `{
+      "importMap": "import_map.json",
+      "compilerOptions": {
+        "jsxFactory": "h"
+      }
+    }
+    `
+  );
+  console.info("Writing import_map.json");
+  await Deno.writeTextFile(
+    "import_map.json",
+    `{
+      "imports": {
+        "react": "https://esm.sh/react",
+        "refuse": "https://raw.githubusercontent.com/shreyassanthu77/refuse/main/refuse.ts"
+      }
+    }`
+  );
+  console.info("Writing main.ts");
+  await Deno.writeTextFile(
+    "main.ts",
+    `import { runApp } from "refuse";
+    import { routes } from "./router.ts";
+    
+    const dev = Deno.args[0] === "dev";
+    
+    await runApp({
+      port: dev ? 3000 : 80,
+      routes,
+      dev,
+    });
+    `
+  );
+  console.info("Creating root layout");
+  await Deno.writeTextFile(
+    "routes/layout.tsx",
+    `import { h } from "refuse";
+
+    const Layout: React.FC = ({ children }) => {
+      return (
+        <html>
+          <head>
+            <title>Refuse</title>
+          </head>
+          <body>{children}</body>
+        </html>
+      );
+    };
+    //
+    export default Layout;
+    `
+  );
+  console.info("Creating index route");
+  await Deno.writeTextFile(
+    "routes/index.ts",
+    `import { h } from "refuse";
+    
+    export default function Index() {
+      return (
+        <main>
+        <h1>Home Page</h1>
+        </main>
+        );
+      }
+      `
+  );
+  console.info("Generating router.ts");
+  await generateRouteMap();
 }
